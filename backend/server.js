15209bc1afcd32c20a8ba1fffb012c99a62a7f62
Kevin <kwang44@buffalo.edu>
2020-04-11 23:51:01 -0400

    adding react stuff

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+import express from 'express';
+import mongoose from 'mongoose';
+
+import imagePostRoutes from './routes/api/imagePost'
 
 var app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/wiogn', { useNewUrlParser: true });
+var db = mongoose.connection;
+
+db.on('error', function () {
+    console.log("MongoDB Error...");
+});
+
+db.once('open', function () {
+    console.log("MongoDB Connection Sucessful!!!");
+});
+
+//Use Routes
+app.use('/api/imageposts', imagePostRoutes);
+
+app.listen(8000, () => console.log('Listening on port 8000..'));
