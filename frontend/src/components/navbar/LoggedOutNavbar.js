991b6b50aa0f06d0a0b86c61cfd82a59538a3ff3
Kevin <kwang44@buffalo.edu>
2020-04-12 02:19:55 -0400

    fix git

---
+++ frontend/src/components/navbar/LoggedOutNavbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './LoggedOutNavbar.css'
+
+function LoggedOutNavbar() {
+    return (
+        <Navbar className='navColor'>
+            <Nav className='mr-auto'>
+                <NavLink exact to='/' className='nav-link navColorChange'>Home</NavLink>
+            </Nav>
+            <Nav>
+                <NavLink exact to='/login' className='nav-link navColorChange'>Log In</NavLink>
+                <NavLink exact to='/signup' className='nav-link navColorChange'>Sign Up</NavLink>
+            </Nav>
+        </Navbar>
+    );
+}
+
+export default LoggedOutNavbar;
